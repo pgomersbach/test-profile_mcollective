6ebc81265fb861a2e0c5632d65167ecce07fbdf1
Paul Gomersbach <p.gomersbach@rely.nl>
2015-07-17 14:10:37 +0200

    add tests

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -16,8 +16,9 @@ describe 'profile_mcollective' do
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to contain_class('profile_mcollective') }
 
-          it { is_expected.to contain_class('profile_mcollective') }
-  
+          it { is_expected.to contain_class('profile_mcollective::params') }
+          it { is_expected.to contain_class('profile_mcollective::client') }
+          it { is_expected.to contain_class('profile_mcollective::server') }
 
         end
       end
